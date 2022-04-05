42e11c56a2b62fa5b315a78341aa9fdcefbc2a7e
ArthurVerrept <Arthur-Verrept@hotmail.co.uk>
2022-04-05 13:38:14 +0100

    add socket connection and connection status

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,38 @@
 import ButtonRepo from '@/components/ButtonRepo.vue'
 import FileUpload from '@/components/FileUpload.vue'
 import SoundFiles from '@/components/SoundFiles.vue'
+import { io } from "socket.io-client";
 
 import * as Tone from 'tone'
-import { onBeforeMount, onMounted, ref, watch } from 'vue'
+import { onBeforeMount, onMounted, ref, watch, reactive } from 'vue'
 
-const velocity = ref(-100)
+
+const socket = io('http://localhost:3000', { query: "name=client" })
+
+const serverVelocity = reactive({ value: 0 })
+const connectedToServer = reactive({ value: false })
+const connectedToArduino = reactive({ value: false })
+
+socket.on('connectedDevices', function(data) {
+  console.log(data)
+  if(data.includes('client')) {
+    connectedToServer.value = true
+  } else {
+    connectedToServer.value = false
+  }
+  if(data.includes('arduino')) {
+    connectedToArduino.value = true
+  } else {
+    connectedToArduino.value = false
+  }
+});
+
+let velocity = ref(-100)
+
+socket.on('velocity', function( data ) {
+  console.log(data)
+  serverVelocity.value = data.velocity
+});
 
 // const activeSound = ref('C4')
 const activeSound = ref('http://localhost:8080/files/redParrott.mp3')
@@ -64,6 +91,13 @@ watch(velocity, async (newQuestion, oldQuestion) => {
   }
 })
 
+function getBackground(bool) {
+  if(!bool) {
+    return 'bg-red-300 pl-2'
+  } 
+  return 'pl-2'
+
+}
 async function start () {
   await Tone.start()
 	console.log('audio is ready')
@@ -81,11 +115,31 @@ async function start () {
       Sound dashboard
       <br />
     </h2>
-    
+    <div class="py-4">
+      <h3
+        class="text-xl leading-9 text-center underline font-bold tracking-tight text-gray-900 sm:leading-10"
+        > 
+           Connections
+      </h3>
+      <div :class="getBackground(connectedToServer.value)">
+        <h3
+        class="text-l leading-9 text-center tracking-tight text-gray-900 sm:leading-10"
+        > 
+          Client: <b> {{ connectedToServer.value }} </b>
+        </h3>
+      </div>
+      <div :class="getBackground(connectedToArduino.value)">
+        <h3
+        class="text-l leading-9 text-center tracking-tight text-gray-900 sm:leading-10"
+        > 
+          Arduino: <b> {{ connectedToArduino.value }} </b>
+        </h3>
+      </div>
+    </div>
     <div>
       <label>Velocity</label>
-      <input v-model="velocity" type="range" min="-100" max="1000"/>
-      {{ velocity }}
+      <input v-model="serverVelocity.value" type="range" min="-100" max="1000"/>
+      {{ serverVelocity.value }}
     </div>
 
     <!-- <div>
